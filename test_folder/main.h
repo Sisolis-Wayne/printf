4d90f46734aa8d37f8e62e6e848e873b7313b6a5
Idoko Angel <angelidokoangel@gmail.com>
2023-07-04 10:29:44 +0000

    update to printf

---
+++ test_folder/main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER
+#define HEADER
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
